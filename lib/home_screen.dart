7bb7c8d4edefd700c51d273968dd38712374ff33
John Liton Mardy <mardy.cse@gmail.com>
2024-03-11 12:39:37 +0600

    Data push in sub branch

---
+++ lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Git & Github', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.indigo,
+    );
+  }
+}
